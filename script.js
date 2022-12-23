14637a4d0676313f994d4065fd30241ad3e42801
ankit5646 <ankityadav564682@gmail.com>
2022-12-23 21:27:09 +0530

    finalized
    we have made the almost project
    and we dont do further things

---
+++ script.js
@@ -0,0 +1,14 @@
+function crousal1() {
+    document.getElementById("1").src="Beautiful JACKED.png";
+}
+setInterval(crousal1,3000);
+
+function crousal2() {
+    document.getElementById("1").src="with your comfort.png";  href="3nd.html";
+}
+setInterval(crousal2,6000);
+
+function crousal3() {
+    document.getElementById("1").src="new arrivals.png";
+}
+setInterval(crousal3,9000);
---
+++ script3.js
@@ -0,0 +1,24 @@
+function crousal1() {
+    document.getElementById("1").src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/61ixrQXrzdL._UY741_.jpg";
+}
+setInterval(crousal1,3000);
+
+function crousal2() {
+    document.getElementById("1").src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/611BV8Ndj+L._UY741_.jpg";
+}
+setInterval(crousal2,6000);
+
+function crousal3() {
+    document.getElementById("1").src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/8183lbxWCHL._UY741_.jpg";
+}
+setInterval(crousal3,9000);
+
+function crousal4() {
+    document.getElementById("1").src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51-wBIeLDhL._UY741_.jpg";
+}
+setInterval(crousal4,12000);
+
+function crousal5() {
+    document.getElementById("1").src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51vb7LJ3oQL._UY741_.jpg";
+}
+setInterval(crousal5,15000);
